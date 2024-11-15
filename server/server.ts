2567661473fd36f74c51d501b89d4c7b607c139f
James McIlraith <jamesmcilraith@gmail.com>
2024-11-16 00:14:56 +1300

    Minor update to comments

--- server/server.ts
+++ server/server.ts
@@ -7,15 +7,6 @@ import treesRoutes from './routes/treesroutes'
 const server = express()
 server.use(express.json())
 
-// Do I put in here CORS (Cross-Origin Resource Sharing) headers for all incoming requests to overcome the Cross-Origin Read Blocking (CORB) blocked a cross-origin response?
-// To solve the image rendering issue from an eternal URL?
-
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Methods", "GET, OPTIONS");
-//   next();
-// });
-
 // ADD YOUR API ROUTES HERE
 server.use('/api/v1/trees', treesRoutes)
 
